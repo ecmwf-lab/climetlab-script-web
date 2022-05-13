424b9c83a541bef0f7b47fc357feba52ff72d5c3
Akshaj Verma <akshajverma7@gmail.com>
2022-05-13 16:21:11 +0530

    Update frontend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "twin.macro"
+import 'styled-components/macro'
+
+const App = () => {
+  return (
+    <div tw="flex flex-col items-center justify-center h-screen w-full">
+        <p tw="bg-green-600 p-8 rounded text-gray-100 text-6xl font-bold">hello esowc!</p>
+    </div>
+  );
+}
+
+export default App;
